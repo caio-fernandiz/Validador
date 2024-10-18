83b187c54c416e579a45e56e8f575789a791ae57
faiocernandes <caio_fernandes@estudante.sc.senai.br>
2024-10-18 16:46:09 -0300

    Validador

--- src/Validador.java
+++ src/Validador.java
@@ -5,6 +5,8 @@ import java.time.Period;
 import java.time.ZoneId;
 import java.util.Date;
 import javax.swing.JOptionPane;
+import java.util.regex.Pattern;
+import java.util.InputMismatchException;
 
 public class Validador {
 
@@ -19,6 +21,13 @@ public class Validador {
      //as informações entre as classes.
     private Janela janela;
     private Dados dados;
+    //Aqui estarei adiconando a parte que irá definir os padrões de um e-mail.
+    //Se o usuário não digitar algo que esteja dentro dos padrões será classificado
+    //como inválido. Eu usei o seguinte site como ajuda. Abaixo deixarei o link:
+    //https://receitasdecodigo.com.br/java/validar-email-em-java
+    private String EMAIL_PATTERN = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z]{2,6}$";
+    private Pattern emailPattern = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
+
 
     //Aqui está o constructor da classe, que recebe a janela e os dados.
     public Validador(Janela janela, Dados dados){
@@ -55,7 +64,7 @@ public class Validador {
                 //"ou". Explicando melhor, se nome ou data não tiver nada, irá receber
                 //um aviso, ou seja, se nome tiver informação mas a data não tem nada, 
                 //o aviso será enviado da mesma forma. 
-                if(nomeDigitado.equals("") || dataDigitada.equals("")){
+                if(nomeDigitado.equals("") || dataDigitada.equals("") || emailDigitado.equals("") || cpfDigitado.equals("")){
 
                     JOptionPane.showMessageDialog(null, "Está falantando informações");
                 }
@@ -77,12 +86,22 @@ public class Validador {
                         JOptionPane.showMessageDialog(null, "Data inválida");
                     }
 
+                    //Então irei fazer o if que irá invalidar o e-mail 
+                    //caso ele não siga os padrões definidos.
+                    if(!isValidEmail(emailDigitado)){
+                        JOptionPane.showMessageDialog(null, "E-mail inválido");
+                    }
+
+                    if(!isCPF(cpfDigitado)){
+                        JOptionPane.showMessageDialog(null, "CPF inválido");
+                    }
+
                     //Esse "else if" é o responsável por validar todas as informações dadas 
                     //pelo usuário caso elas estejam de acordo com o esperado e não sejam 
                     //barradas por nenhum dos "if" anteriores. Isso é, se forem corretamente 
-                    //digitados. Ainda irei fazer a validação caso não estejam de acordo com 
-                    //outras regras.
-                    else if(nomeDigitado.matches(".*[\\p{L}]+\\s+[\\p{L}]+.*") && dataDigitada.matches("\\d{2}/\\d{2}/\\d{4}")){
+                    //digitados. A data ainda irá precisar de mais uma validação, a de idade.
+                    //Será feita dentro desse mesmo "else if".
+                    else if(nomeDigitado.matches(".*[\\p{L}]+\\s+[\\p{L}]+.*") && dataDigitada.matches("\\d{2}/\\d{2}/\\d{4}") && isValidEmail(emailDigitado) && isCPF(cpfDigitado)){
 
                         //Esse if é para conferir se a data é válida mesmo que ela
                         //tenha sido digitda da maneira correta.
@@ -134,9 +153,15 @@ public class Validador {
                         //E aqui a string chamada "nomeDigitado" irá enviar 
                         //a informação para a várivael "nome" na classe "Dados".
                         dados.setNome(nomeDigitado);
+                        dados.setEmail(emailDigitado);
+                        dados.setCPF(cpfDigitado);
 
                         janela.getJlRecebeNome().setText(dados.getNome());
                         janela.getJlRecebeData().setText(dados.getData());
+                        janela.getJlRecebeEmail().setText(dados.getEmail());
+                        //o CPF precisa do "imprimeCPF" pois assim irá aparecer formatado
+                        //como um CPF comum.
+                        janela.getJlRecebeCPF().setText(imprimeCPF(dados.getCPF()));
                         }
                         }
 
@@ -144,6 +169,79 @@ public class Validador {
                 }
             }
         });
-
     }
+
+    //Aqui estou criando um metódo chamado "isValidEmail". 
+    //Isso aqui irá servir para validar o e-mail, como o 
+    //nome já indica.
+    public boolean isValidEmail(String email) {
+    String emailRegex = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+    Pattern pattern = Pattern.compile(emailRegex);
+    return pattern.matcher(email).matches();
+    }
+
+    //Agore irei adicionar um metódo que será responsável pela validação do cpf.
+    //O link que eu peguei esse código vai estar abaixo, ele explica melhor 
+    //essa parte.
+    //https://www.devmedia.com.br/validando-o-cpf-em-uma-aplicacao-java/22097
+    public static boolean isCPF(String CPF) {
+        // considera-se erro CPF"s formados por uma sequencia de numeros iguais
+        if (CPF.equals("00000000000") ||
+            CPF.equals("11111111111") ||
+            CPF.equals("22222222222") || CPF.equals("33333333333") ||
+            CPF.equals("44444444444") || CPF.equals("55555555555") ||
+            CPF.equals("66666666666") || CPF.equals("77777777777") ||
+            CPF.equals("88888888888") || CPF.equals("99999999999") ||
+            (CPF.length() != 11))
+            return(false);
+
+        char dig10, dig11;
+        int sm, i, r, num, peso;
+
+        // "try" - protege o codigo para eventuais erros de conversao de tipo (int)
+        try {
+        // Calculo do 1o. Digito Verificador
+            sm = 0;
+            peso = 10;
+            for (i=0; i<9; i++) {
+        // converte o i-esimo caractere do CPF em um numero:
+        // por exemplo, transforma o caractere "0" no inteiro 0
+        // (48 eh a posicao de "0" na tabela ASCII)
+            num = (int)(CPF.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                dig10 = '0';
+            else dig10 = (char)(r + 48); // converte no respectivo caractere numerico
+
+        // Calculo do 2o. Digito Verificador
+            sm = 0;
+            peso = 11;
+            for(i=0; i<10; i++) {
+            num = (int)(CPF.charAt(i) - 48);
+            sm = sm + (num * peso);
+            peso = peso - 1;
+            }
+
+            r = 11 - (sm % 11);
+            if ((r == 10) || (r == 11))
+                 dig11 = '0';
+            else dig11 = (char)(r + 48);
+
+        // Verifica se os digitos calculados conferem com os digitos informados.
+            if ((dig10 == CPF.charAt(9)) && (dig11 == CPF.charAt(10)))
+                 return(true);
+            else return(false);
+                } catch (InputMismatchException erro) {
+                return(false);
+            }
+        }
+
+        public static String imprimeCPF(String CPF) {
+            return(CPF.substring(0, 3) + "." + CPF.substring(3, 6) + "." +
+            CPF.substring(6, 9) + "-" + CPF.substring(9, 11));
+        }
 }
